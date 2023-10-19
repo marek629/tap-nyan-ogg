9058600ebe23a6d627d3ad6c6df92eabb4b028d0
Marek Jędryka <jedryka89@gmail.com>
2023-10-19 17:57:53 +0200

    fix: test failed randomly by to short timeout at waiting for input stream

--- src/cli.js
+++ src/cli.js
@@ -67,7 +67,7 @@ const observer = new TapObserver()
 pipeline([process.stdin, observer, process.stdout], () => {})
 
 try {
-  await eventTimeout(observer, 'start', 90)
+  await eventTimeout(observer, 'start', 165)
 } catch (r) {
   console.error(
     'Empty input stream! A text stream formatted according to the Test Anything Protocol was expected.',
